f02f85a713c7db171af347543f1ba6e8c7c28f2b
Sanjay-Kumar-R-A <rasanjaykumarsk@gmail.com>
2024-12-27 22:09:08 +0530

    path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SearchPage from './pages/SearchPage';
-import MovieDetailsPage from './pages/MovieDetailsPage';
+import SearchPage from './Pages/SearchPage';
+import MovieDetailsPage from './Pages/MovieDetailsPage';
 
 const App = () => {
   return (
